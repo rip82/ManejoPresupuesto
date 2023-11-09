386bd92c9a09d8069c3b8bb552ef13aaff0a7b80
Daniel Rivera <daniel.rivera.1989@gmail.com>
2023-11-09 18:54:10 -0300

    ultimo

---
+++ ManejoPresupuesto/Servicios/RepositorioCategorias.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using ManejoPresupuesto.Models;
+using Microsoft.Data.SqlClient;
+
+namespace ManejoPresupuesto.Servicios
+{
+    public interface IRepositorioCategorias
+    {
+        Task CrearCategoria(Categoria categoria);
+    }
+    public class RepositorioCategorias: IRepositorioCategorias
+    {
+        private readonly string connectionString;
+        public RepositorioCategorias(IConfiguration configuration)
+        {
+            connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public async Task CrearCategoria(Categoria categoria)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var id = await connection.QuerySingleAsync<int>(@"
+                                            INSERT INTO Categorias (Nombre, TipoOperacionId, UsuarioId) 
+                                            VALUES (@Nombre, @TipoOperacionId, @UsuarioId);
+
+                                            SELECT SCOPE_IDENTITY();
+                                            ", categoria);
+            categoria.Id = id;
+        }
+    }
+}
